ca49c27bb5fb1ba1ba1e58717a2fbe7f288e137b
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-04 11:25:51 -0300

    Initial Commit

---
+++ HabitTrackerTests/HabitTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerTests.swift
+//  HabitTrackerTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 04/02/2025.
+//
+
+import Testing
+@testable import HabitTracker
+
+struct HabitTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
